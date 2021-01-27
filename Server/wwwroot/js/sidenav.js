fe84e9f8769f3010ffa20696bfef260e9857e0bd
senshi111 <40372242+Senshi111@users.noreply.github.com>
2021-01-27 19:39:30 +0100

    add dark-light theme

---
+++ Server/wwwroot/js/sidenav.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+const sidenav = document.getElementById('sidenav-1');
+const sidenavInstance = new mdb.Sidenav(sidenav);
+let innerWidth = null;
+
+const setMode = (e) => {
+    // Check necessary for Android devices
+    if (window.innerWidth === innerWidth) {
+        return;
+    }
+
+    innerWidth = window.innerWidth;
+
+    if (window.innerWidth < 660) {
+        sidenavInstance.changeMode('over');
+        sidenavInstance.hide();
+    } else {
+        sidenavInstance.changeMode('side');
+        sidenavInstance.show();
+    }
+};
+
+setMode();
+
+// Event listeners
+
+window.addEventListener('resize', setMode);
+
+document.getElementById('slim-toggler').addEventListener('click', () => {
+    sidenavInstance.toggleSlim();
+})
